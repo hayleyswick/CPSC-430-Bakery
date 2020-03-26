778a32c5bc4225fb0acb160e64ba4001b779fa5f
Michael Corbin <mwc2236@gmail.com>
2020-03-26 17:42:00 -0400

    basic setup for Logs.py, Order.py is empty

---
+++ Logs.py
@@ -0,0 +1,22 @@
+import Order
+
+class Logs:
+
+    logs[]
+
+
+
+    def GetLogs():
+        return logs
+
+    def EditLog(logNum, changes):
+
+        order = Order(changes)
+
+        log[logNum] = order
+
+        def DisplayConfirmation():
+            
+            do a thing
+
+    
