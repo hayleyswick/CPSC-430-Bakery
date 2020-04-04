fdd8dcb35d4d0ec68a4ab4bb680d84f5627dd346
dosdude1 <dosdude@netzero.net>
2020-04-03 20:52:19 -0400

    DB schema changes

--- bakery_backend/bakery.sql
+++ bakery_backend/bakery.sql
@@ -4,6 +4,7 @@ USE bakery_calc;
 
 CREATE TABLE users (id VARCHAR(45) PRIMARY KEY NOT NULL, username TEXT NOT NULL, password TEXT, type TEXT, firstname TEXT, lastname TEXT);
 CREATE TABLE sessions (session_id VARCHAR(45) PRIMARY KEY NOT NULL, user_id VARCHAR(45) NOT NULL);
-CREATE TABLE inventory (batter_type TEXT, quantity INT);
-CREATE TABLE order_details (order_number INT NOT NULL, batter_type TEXT, quantity INT);
+CREATE TABLE inventory (batter_type TEXT, quantity DOUBLE);
+CREATE TABLE order_details (order_number INT NOT NULL, batter_type TEXT, cake_type TEXT, quantity DOUBLE);
 CREATE TABLE orders (order_number INT PRIMARY KEY NOT NULL AUTO_INCREMENT, customer_firstname TEXT, customer_lastname TEXT, order_date DATE);
+
